845cdfc8aa9f54f1ce3fa8c23d5de1282773c040
Pavel <paveliv@gmail.com>
2019-09-09 23:00:51 +0300

    Intermediate changes to all projects page

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,12 @@ const WT = require("@waves/waves-transactions")
 const ORACLE_ADDRESS = "3N8QtDWHjwjE6uuwEXyKf3kRVkC8sjvV8EY"
 const API_HOST = "https://nodes-testnet.wavesnodes.com"
 
+const asyncForEach = async (array, callback) => {
+	for (let index = 0; index < array.length; index++) {
+		await callback(array[index], index, array)
+	}
+}
+
 exports.createPages = async ({ actions: { createPage } }) => {
 	const allProjects = await WT.nodeInteraction
 		.accountData(ORACLE_ADDRESS, API_HOST)
@@ -10,11 +16,28 @@ exports.createPages = async ({ actions: { createPage } }) => {
 			return res
 		})
 
+	let getAllProjectsData = async allProjects => {
+		let allProjectsData = {}
+		const allProjectsArray = Object.values(allProjects)
+		await asyncForEach(allProjectsArray, async project => {
+			let address = project.key.split("_")[0]
+			await WT.nodeInteraction.accountData(address, API_HOST).then(res => {
+				allProjectsData = {
+					...allProjectsData,
+					[address]: res,
+				}
+			})
+		})
+		return allProjectsData
+	}
+
+	const allProjectsData = await getAllProjectsData(allProjects)
+
 	// Create a page that lists all Projects.
 	createPage({
 		path: `/projects/`,
 		component: require.resolve("./src/page-templates/all-projects.template.js"),
-		context: { allProjects },
+		context: { allProjectsData },
 	})
 
 	// Create a page for each Project.
