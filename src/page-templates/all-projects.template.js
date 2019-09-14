087bcca662a1313fc0dd5fc0200cc82db0959acd
Pavel <paveliv@gmail.com>
2019-09-14 17:43:44 +0300

    intermidiate commit hacakathon

--- src/page-templates/all-projects.template.js
+++ src/page-templates/all-projects.template.js
@@ -1,14 +1,13 @@
-import React, { Fragment } from "react"
+import React, { Fragment } from "react";
 import hashicon from "hashicon";
-import { withFallback } from "../cms"
-import { SEO, Heading, Header } from "../components"
-import { Box,Flex,Text,Card } from "rebass"
-import { AppLayout } from "../app-layout.component"
-import { Link } from "../components/link.component"
+import { withFallback } from "../cms";
+import { SEO, Heading, Header } from "../components";
+import { Box, Flex, Text, Card } from "rebass";
+import { AppLayout } from "../app-layout.component";
+import { Link } from "../components/link.component";
 
-const title = "All projects"
+const title = "All projects";
 const linkBasic = "/projects/";
-
 
 export const AllProjectsTemplate = ({ pageContext: allProjects }) => {
 	return (
@@ -17,42 +16,49 @@ export const AllProjectsTemplate = ({ pageContext: allProjects }) => {
 			<Fragment>
 				<Flex>
 					{Object.keys(allProjects.allProjectsData).map(item => {
-						return <ProjectList pageContext={allProjects.allProjectsData[item]} itemKey={item}/>
+						return (
+							<ProjectList
+								pageContext={allProjects.allProjectsData[item]}
+								itemKey={item}
+							/>
+						);
 					})}
 				</Flex>
 			</Fragment>
 		</article>
-	)
-}
+	);
+};
 // foreach jsx
 //	{JSON.stringify(allProjects.allProjectsData)}
-export const ProjectList = ({ pageContext: projects, itemKey: key}) => {
+export const ProjectList = ({ pageContext: projects, itemKey: key }) => {
 	return (
-				<Box width={1/4} p={1}>
-					<Card>
-						<Flex>
-							
-							<Box width={1} p={1}>
-								<center> <img alt={key} title={key} src={hashicon(key).toDataURL()} />	</center>
-								<Heading fontSize={[3, 4]} color="primary">
-									<Link to={'/projects/'.concat(key)} >{projects.fundraiseName.value}</Link>
-								</Heading>
-								<Text>
-									by: {projects.fundraiseDescription.value}
-								</Text>	
-								<Text>
-									<br/>
-									by: {projects.eResidentName.value}
-								</Text>	
-							</Box>
-						
-						</Flex>
-					</Card>
-				</Box>
-		
-		
-	)
-}
+		<Box width={1 / 4} p={1}>
+			<Card>
+				<Flex>
+					<Box width={1} p={1}>
+						<center>
+							{typeof document !== `undefined` ? (
+								<img alt={key} title={key} src={hashicon(key).toDataURL()} />
+							) : (
+								""
+							)}
+						</center>
+						<Heading fontSize={[3, 4]} color="primary">
+							<Link to={"/projects/".concat(key)}>
+								{projects.fundraiseName.value}
+							</Link>
+						</Heading>
+						<Text>by: {projects.fundraiseDescription.value}</Text>
+						<Text>
+							<br />
+							by: {projects.eResidentName.value}
+						</Text>
+					</Box>
+				</Flex>
+			</Card>
+		</Box>
+	);
+};
 //<Link to={'/projects/'.concat(key)} >sdfsdfsdf</Link>
 
 const AllProjects = ({ pageContext: allProjects }) => {
@@ -60,7 +66,7 @@ const AllProjects = ({ pageContext: allProjects }) => {
 		<AppLayout>
 			<AllProjectsTemplate pageContext={allProjects} />
 		</AppLayout>
-	)
-}
+	);
+};
 
-export default AllProjects
+export default AllProjects;
