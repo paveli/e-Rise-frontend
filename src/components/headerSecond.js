972d81a9da205ea9fdd41cd16f29d4398db97f1e
Pavel <paveliv@gmail.com>
2019-09-14 18:54:56 +0300

    Second header added

---
+++ src/components/headerSecond.js
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import { React, Fragment } from "react";
+import { Flex, Box, Heading } from "rebass";
+
+import { Menu } from "./menu.component";
+import { Link } from "./link.component";
+import Auth from "./auth";
+
+export const HeaderSecond = ({ siteTitle }) => (
+	<Fragment>
+		<header
+			style={{
+				marginBottom: `1.45rem`
+			}}
+		>
+			<Flex>
+				<Box p={3} width={1 / 4} color="white">
+					<Heading fontSize={[5, 6]} color="primary">
+						<Link
+							to="/"
+							style={{
+								color: `#0000F0`,
+								textDecoration: `none`
+							}}
+						>
+							{" "}
+							{siteTitle}
+						</Link>
+					</Heading>
+				</Box>
+				<Box p={3} width={3 / 4} textAlign="right">
+					<Menu type="second" /> {"  "} <Auth />
+				</Box>
+			</Flex>
+		</header>
+	</Fragment>
+);
+
+HeaderSecond.propTypes = {
+	siteTitle: PropTypes.string
+};
+
+HeaderSecond.defaultProps = {
+	siteTitle: ``
+};
--- src/components/menu.component.js
+++ src/components/menu.component.js
@@ -1,37 +1,53 @@
-import { React, Fragment } from "react"
-import { graphql, StaticQuery } from "gatsby"
-import { Link } from "./link.component"
-import { safelyGetSiteConfig } from "../cms"
+import { React, Fragment } from "react";
+import { graphql, StaticQuery } from "gatsby";
+import { Link } from "./link.component";
+import { safelyGetSiteConfig } from "../cms";
 
 export const query = graphql`
-  query {
-    sitePage(path: { eq: "/config/" }) {
-      context {
-        frontmatter {
-          menu_nav {
-            text
-            url
-          }
-        }
-      }
-    }
-  }
-`
+	query {
+		sitePage(path: { eq: "/config/" }) {
+			context {
+				frontmatter {
+					menu_nav {
+						text
+						url
+					}
+				}
+			}
+		}
+	}
+`;
 
-export const Menu = () => (
-  <StaticQuery
-    query={query}
-    render={data => {
-      const menu = safelyGetSiteConfig(data.sitePage).menu_nav || []
-      return (
-        <Fragment>
-          {menu.map((item, i) => (
-            <Link variant="nav" to={item.url} key={i} style={{color: `white`, textDecoration: `none`}}>
-              {item.text}
-            </Link>
-          ))}
-        </Fragment>
-      )
-    }}
-  />
-)
+export const Menu = props => (
+	<StaticQuery
+		query={query}
+		render={data => {
+			const menu = safelyGetSiteConfig(data.sitePage).menu_nav || [];
+			return (
+				<Fragment>
+					{menu.map((item, i) =>
+						props.type == "second" ? (
+							<Link
+								variant="nav"
+								to={item.url}
+								key={i}
+								style={{ textDecoration: `none` }}
+							>
+								{item.text}
+							</Link>
+						) : (
+							<Link
+								variant="nav"
+								to={item.url}
+								key={i}
+								style={{ color: `white`, textDecoration: `none` }}
+							>
+								{item.text}
+							</Link>
+						)
+					)}
+				</Fragment>
+			);
+		}}
+	/>
+);
