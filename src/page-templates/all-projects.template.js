c64b8f4ff429bcd253e74a7b67e9af0f1db096cc
victor6161 <victorkozlov120695@gmail.com>
2019-09-14 09:29:32 +0300

    add all projects item

--- src/page-templates/all-projects.template.js
+++ src/page-templates/all-projects.template.js
@@ -1,49 +1,59 @@
-import React from "react"
-
+import React, { Fragment } from "react"
+import hashicon from "hashicon";
 import { withFallback } from "../cms"
 import { SEO, Heading } from "../components"
-import { Box,Flex,Text } from "rebass"
+import { Box,Flex,Text,Card } from "rebass"
 import { AppLayout } from "../app-layout.component"
 import { Link } from "../components/link.component"
 
 const title = "All projects"
 const linkBasic = "/projects/";
 
+
 export const AllProjectsTemplate = ({ pageContext: allProjects }) => {
 	return (
 		<article>
 			<SEO title={withFallback(title, "")} />
 			<Heading tag={1}>{title}</Heading>
-			// foreach jsx
 			{JSON.stringify(allProjects.allProjectsData)}
-			
 			{Object.keys(allProjects.allProjectsData).map(item => {
 				return <ProjectList pageContext={allProjects.allProjectsData[item]} itemKey={item}/>
 			})}
 		</article>
 	)
 }
-
+// foreach jsx
+//
 export const ProjectList = ({ pageContext: projects, itemKey: key}) => {
 	return (
-		<Flex mx={-2}>
-				<Box width={1/2} px={2}>
-				<Text p={1} color='background' bg='primary'>
-					{projects.eResidentName.value}
-				</Text>
+		<Fragment>
+		<Flex>
+				<Box width={1/4} p={1}>
+					<Card>
+						<Flex>
+							
+							<Box width={1} p={1}>
+								<center> <img alt={key} title={key} src={hashicon(key).toDataURL()} />	</center>
+								<Heading fontSize={[3, 4]} color="primary">
+									<Link to={'/projects/'.concat(key)} >{projects.fundraiseName.value}</Link>
+								</Heading>
+								<Text>
+									by: {projects.fundraiseDescription.value}
+								</Text>	
+								<Text>
+									by: {projects.eResidentName.value}
+								</Text>	
+							</Box>
+						
+						</Flex>
+					</Card>
 				</Box>
-				<Box width={1/2} px={2}>
-				<Text p={1} color='background' bg='primary'>
-					{key}
-				</Text>
-				<Link to={'/projects/'.concat(key)} >sdfsdfsdf</Link>
-				</Box>
-				
-				
 		</Flex>
+		</Fragment>
+		
 	)
 }
-
+//<Link to={'/projects/'.concat(key)} >sdfsdfsdf</Link>
 
 const AllProjects = ({ pageContext: allProjects }) => {
 	return (
